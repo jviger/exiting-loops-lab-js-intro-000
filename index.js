31e82c6c71b8666124f3de1ac83e2cc5fe3eb4fd
Jason Viger <jason.viger@gmail.com>
2018-07-10 16:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array, findFn){
   for(let i = 0; i < array.length; i++){
-    if(array[i] == findFn){
+    if(findFn(array[i])){
      return array[i];
     } 
   }
