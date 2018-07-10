012188c168876e04e403c157fde0bdc3efeb2a65
Jason Viger <jason.viger@gmail.com>
2018-07-10 15:29:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function breakOut(array, changeValue, stopValue){
+  
+  for(let i = 0; i < array.length; i++){
+    
+    if(array[i] === stopValue){
+      break;
+    } else { 
+      array[i] = changeValue;
+      
+    }
+    
+  }
+  
+}
